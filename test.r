283bcf707feaaee088361c393859305b47b176e1
kaixin-r <wish.there.is.something.true@gmail.com>
2020-08-01 20:43:14 +0900

    library infomation updated

--- test.r
+++ test.r
@@ -1,3 +1,7 @@
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(tidyverse, ggplot2)
+
+
 plot(iris$Petal.Length, iris$Petal.Width)
 result <- lm(Sepal.Length ~ Sepal.Width, data = iris)
 summary(result)
